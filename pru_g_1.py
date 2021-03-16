e38b56ed67fce0d21eb42427eed6d44c431dcde9
laura <la.calle.de.la.piruleta@hotmail.com>
2021-03-16 19:43:49 +0100

    mi primer commit

---
+++ pru_g_1.py
@@ -0,0 +1,20 @@
+print ('¿Hola qué tal?')
+a=10
+b=1
+for i in range(a):
+    if b==1:
+        b=(b*i)+i
+    else:
+        print(b)
+        b=(b*i)+i
+   
+    
+print()
+print('-'*63)
+print('='*20, 'SÚPER RESULTADO FINAL', '='*20)
+print('-'*63)
+print()
+print('b es igual a:', b)
+print ()
+print('Lalala')
+print()
